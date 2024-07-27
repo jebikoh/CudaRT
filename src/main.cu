5dbfefb2eb2753d7c99a17066df666a9b85837e8
Jayden E. <aneeshedara@gmail.com>
2024-07-27 16:06:29 -0400

    Add basic AA

--- src/main.cu
+++ src/main.cu
@@ -27,6 +27,16 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
     }
 }
 
+
+__device__ jtx::Vec3f rayColor(const jtx::Rayf &r, Hittable **world) {
+    HitRecord rec;
+    if ((*world)->hit(r, {0.0f, jtx::INFINITY_F}, rec)) {
+        return 0.5f * (rec.normal + 1.0f);
+    }
+    float t = 0.5f * (jtx::normalize(r.dir).y + 1.0f);
+    return jtx::lerp(jtx::Vec3f{1.0f, 1.0f, 1.0f}, jtx::Vec3f{0.5f, 0.7f, 1.0f}, t);
+}
+
 __global__ void createWorld(Hittable **d_list, Hittable **d_world) {
     if (threadIdx.x == 0 && blockIdx.x == 0) {
         d_list[0] = new Sphere(jtx::Point3f{0, 0, -1}, 0.5);
@@ -41,15 +51,6 @@ __global__ void freeWorld(Hittable **d_list, Hittable **d_world) {
     delete *d_world;
 }
 
-__device__ jtx::Vec3f rayColor(const jtx::Rayf &r, Hittable **world) {
-    HitRecord rec;
-    if ((*world)->hit(r, {0.0f, jtx::INFINITY_F}, rec)) {
-        return 0.5f * (rec.normal + 1.0f);
-    }
-    float t = 0.5f * (jtx::normalize(r.dir).y + 1.0f);
-    return jtx::lerp(jtx::Vec3f{1.0f, 1.0f, 1.0f}, jtx::Vec3f{0.5f, 0.7f, 1.0f}, t);
-}
-
 __global__ void renderInit(int maxX, int maxY, curandState *randState) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
@@ -61,6 +62,7 @@ __global__ void renderInit(int maxX, int maxY, curandState *randState) {
 __global__ void render(RGB8 *fb,
                        int maxX,
                        int maxY,
+                       int samplesPerPixel,
                        jtx::Vec3f lowerLeft,
                        jtx::Vec3f horizontal,
                        jtx::Vec3f vertical,
@@ -72,10 +74,14 @@ __global__ void render(RGB8 *fb,
     if((i >= maxX) || (j >= maxY)) return;
     int pixel_index = j * maxX + i;
     curandState localRandState = randState[pixel_index];
-    float u = float(i) / float(maxX);
-    float v = float(j) / float(maxY);
-    jtx::Rayf r(origin, lowerLeft + u * horizontal + v * vertical);
-    Color color = rayColor(r, world);
+    Color color(0, 0, 0);
+    for (int s = 0; s < samplesPerPixel; ++s) {
+        float u = (float(i) + curand_uniform(&localRandState)) / float(maxX);
+        float v = (float(j) + curand_uniform(&localRandState)) / float(maxY);
+        jtx::Rayf r(origin, lowerLeft + u * horizontal + v * vertical);
+        color += rayColor(r, world);
+    }
+    color /= float(samplesPerPixel);
     fb[pixel_index].x = uint8_t(256 * jtx::clamp(color.r, 0.0f, 0.999f));
     fb[pixel_index].y = uint8_t(256 * jtx::clamp(color.g, 0.0f, 0.999f));
     fb[pixel_index].z = uint8_t(256 * jtx::clamp(color.b, 0.0f, 0.999f));
@@ -86,6 +92,7 @@ int main() {
     const int ny = 600;
     const int tx = 8;
     const int ty = 8;
+    const int samplesPerPixel = 50;
 
     std::cerr << "Rendering a " << nx << "x" << ny << " image ";
     std::cerr << "in " << tx << "x" << ty << " blocks.\n";
@@ -113,7 +120,7 @@ int main() {
 
     clock_t start, stop;
     start = clock();
-    render<<<blocks, threads>>>(fb, nx, ny,
+    render<<<blocks, threads>>>(fb, nx, ny, samplesPerPixel,
                                 jtx::Vec3f(-2.0, -1.0, -1.0),
                                 jtx::Vec3f(4.0, 0.0, 0.0),
                                 jtx::Vec3f(0.0, 2.0, 0.0),
